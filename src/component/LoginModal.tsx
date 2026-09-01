7a4654c94aba9b219f5e1b1251fe153294fd3a8e
franvinkus <vincent.kurnia@tms-consulting.co.id>
2026-09-01 11:15:48 +0700

    refactor: revamp login and register modal UI

--- src/component/LoginModal.tsx
+++ src/component/LoginModal.tsx
@@ -65,22 +65,33 @@ export default function LoginModal({onClose}: param) {
     }
 
     return(
-        <div>
+        <div className="fixed inset-0 bg-black/60 backdrop-blur-sm z-40">
             {loggingIn? (
                 //Login
-                <div className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[390px] w-[400px] fixed border-2 bg-white rounded-xl p-4">
-                    <div className="flex-cols">
-                        <div className="flex justify-center text-2xl">
-                            <div>
-                                Login
+                <div className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-auto w-[90%] max-w-[550px] fixed bg-white border-1 border-gray-500 rounded-xl p-4 z-100">
+                    <div className="flex-col">
+                        <div className="flex flex-col justify-start gap-y-2">
+                            <div className="text-lg opacity-50">
+                                Welcome Back
+                            </div>
+                            <div className="text-2xl opacity-100">
+                                Login to Clear
+                            </div>
+                            <div className="text-md opacity-50">
+                                Keep planning and move forward
                             </div>
                         </div>
 
-                        <div className="flex-row justify space-y-7 mt-10">
+                        <form 
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            handleSubmit();
+                        }} 
+                        className="flex-row justify space-y-7 mt-10">
                             <div className="flex flex-col">
-                                <label className="mb-2">Username: </label>
+                                <label className="mb-2 text-gray-400 font-semibold text-sm">Username </label>
                                 <input
-                                className="border-2 border-black rounded-xl p-3 outline-none focus:ring-2 focus:ring-black"
+                                className="border-1 border-gray-500 rounded-xl p-3 outline-none focus:ring-2 focus:ring-gray-500"
                                 placeholder="Insert username"
                                 value={username}
                                 onChange={(e) => setUsername(e.target.value)}
@@ -88,51 +99,66 @@ export default function LoginModal({onClose}: param) {
                             </div>
 
                             <div className="flex flex-col">
-                                <label className="mb-2">Password: </label>
+                                <label className="mb-2 text-gray-400 font-semibold text-sm">Password </label>
                                 <input
-                                className="border-2 border-black rounded-xl p-3 outline-none focus:ring-2 focus:ring-black"
+                                className="border-1 border-gray-500 rounded-xl p-3 outline-none focus:ring-2 focus:ring-gray-500"
                                 placeholder="Insert Password"
                                 type="password"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
                                 />
                             </div>
-                        </div>
+                        </form>
 
-                        <div className="flex justify-between items-center mt-6">
+                        <div className="flex flex-col justify-center items-center mt-6 gap-y-10">
                             <button 
-                            className="text-sm text-blue-400 hover:cursor-pointer hover:text-blue-300"
-                            onClick={() => setIsLoggingIn(false)}
-                            >
-                                Don&apos;t have an account? Register, Here!
-                            </button>
-
-                            <button 
-                            className="border-2 border-black rounded-xl p-3 hover:bg-black hover:border-white hover:text-white hover:cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
+                            className=" w-full border-2 text-white bg-green-700 rounded-lg px-3 py-1 hover:bg-green-900 hover:cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
                             onClick={handleSubmit}
                             disabled={isLoading}
                             >
-                                Submit
+                                Login
                             </button>
+
+                            <div className="text-sm opacity-80">
+                                Don&apos;t have an account?
+                                <button 
+                                className="text-sm text-green-900 hover:cursor-pointer hover:underline ml-2"
+                                onClick={() => setIsLoggingIn(false)}
+                                >
+                                     Register, Here!
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
 
             ) : (
                 //Register
-                <div className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[480px] w-[400px] fixed border-2 bg-white rounded-xl p-4">
-                    <div className="flex-cols">
-                        <div className="flex justify-center text-2xl">
-                            <div>
-                                Register
+                <div className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-auto w-[90%] max-w-[550px] fixed bg-white border-1 border-gray-500 rounded-xl p-4 z-100">
+                    <div className="flex-col">
+
+                        <div className="flex flex-col justify-start gap-y-2">
+                            <div className="text-lg opacity-50">
+                                Ready to Start Planning?
+                            </div>
+                            <div className="text-2xl opacity-100">
+                                Register Account
+                            </div>
+                            <div className="text-md opacity-50">
+                                Keep planning and move forward
                             </div>
                         </div>
 
-                        <div className="flex-row justify space-y-7 mt-8">
+                        <form 
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            handleSubmit();
+                        }}
+                        className="flex-row justify space-y-7 mt-8">
                             <div className="flex flex-col">
-                                <label className="mb-2">Username: </label>
+                                <label className="mb-2 text-gray-400 font-semibold text-sm">Username </label>
                                 <input
-                                className="border-2 border-black rounded-xl p-3 outline-none focus:ring-2 focus:ring-black"
+                                className="border-1 border-gray-500 rounded-xl p-3 outline-none focus:ring-2 focus:ring-gray-500"
                                 placeholder="Insert username"
                                 value={username}
                                 onChange={(e) => setUsername(e.target.value)}
@@ -140,9 +166,9 @@ export default function LoginModal({onClose}: param) {
                             </div>
 
                             <div className="flex flex-col">
-                                <label className="mb-2">Email: </label>
+                                <label className="mb-2 text-gray-400 font-semibold text-sm">Email </label>
                                 <input
-                                className="border-2 border-black rounded-xl p-3 outline-none focus:ring-2 focus:ring-black"
+                                className="border-1 border-gray-500 rounded-xl p-3 outline-none focus:ring-2 focus:ring-gray-500"
                                 placeholder="Insert Email"
                                 type="email"
                                 value={email}
@@ -151,32 +177,35 @@ export default function LoginModal({onClose}: param) {
                             </div>
 
                             <div className="flex flex-col">
-                                <label className="mb-2">Password: </label>
+                                <label className="mb-2 text-gray-400 font-semibold text-sm">Password </label>
                                 <input
-                                className="border-2 border-black rounded-xl p-3 outline-none focus:ring-2 focus:ring-black"
+                                className="border-1 border-gray-500 rounded-xl p-3 outline-none focus:ring-2 focus:ring-gray-500"
                                 placeholder="Insert Password"
                                 type="password"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
                                 />
                             </div>
-                        </div>
+                        </form>
                 
-                        <div className="flex justify-between items-center mt-6">
+                        <div className="flex flex-col justify-center items-center mt-6 gap-y-10">
                             <button 
-                            className="text-sm text-blue-400 hover:cursor-pointer hover:text-blue-300"
-                            onClick={() => setIsLoggingIn(true)}
-                            >
-                                Login
-                            </button>
-
-                            <button 
-                            className="border-2 border-black rounded-xl p-3 hover:bg-black hover:border-white hover:text-white hover:cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
+                            className=" w-full border-2 text-white bg-green-700 rounded-lg px-3 py-1 hover:bg-green-900 hover:cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
                             onClick={handleSubmit}
                             disabled={isLoading}
                             >
-                                Submit
+                                Register
                             </button>
+
+                            <div className="text-sm opacity-80">
+                                Already have an account?
+                                <button 
+                                className="text-sm text-green-900 hover:cursor-pointer hover:underline ml-2"
+                                onClick={() => setIsLoggingIn(true)}
+                                >
+                                     Login, Here!
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
